b9d951033a3898ace4dde9475d5e3e247b5618c7
jayantKumar2001 <jayant.2023@iic.ac.in>
2024-07-14 11:29:56 +0530

    fixed store issue

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const store = MongoStore.create({
     touchAfter: 24*3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
     console.log("ERROR in MONGO SESSION STORE", err);
 })
 
